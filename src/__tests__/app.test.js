ecfe270f73e880f4a88a843827d1b35476ae078f
Ditlef <ditlef.diseth@schibsted.com>
2021-08-15 21:47:12 +0200

    Create tests for SearchPage

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -4,100 +4,201 @@
 
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import MyApp from '../pages/_app';
-import HomePage from '../pages/products/[page]';
+import ProductPage from '../pages/products/[page]';
+import SearchPage from '../pages/products/search';
 
-test('Should display a button to open cart', () => {
-    const pageProps = { products: [] };
-    render(
-        <MyApp
-            pageProps={pageProps}
-            Component={HomePage}
-            cart={{ id: 1, products: {} }}
-        />,
-    );
+describe('Display page', () => {
+    test('Should display a button to open cart', () => {
+        const pageProps = { products: [] };
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={ProductPage}
+                cart={{ id: 1, products: {} }}
+            />,
+        );
 
-    expect(screen.getByText('Cart (0)')).toBeInTheDocument();
-});
+        expect(screen.getByText('Cart (0)')).toBeInTheDocument();
+    });
 
-test('Should add item to cart if "add to bag" is clicked', async () => {
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-            ok: true,
-        }),
-    );
+    test('Should add item to cart if "add to bag" is clicked', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+            }),
+        );
 
-    const pageProps = {
-        products: [
-            {
-                id: 1,
-                name: 'Product',
-                description: 'Description',
-                defaultImage: '',
-                images: [],
-                price: 1234,
-                discount: 0,
-            },
-        ],
-    };
-    render(
-        <MyApp
-            pageProps={pageProps}
-            Component={HomePage}
-            cart={{ id: 1, products: {} }}
-        />,
-    );
+        const pageProps = {
+            products: [
+                {
+                    id: 1,
+                    name: 'Product',
+                    description: 'Description',
+                    defaultImage: '',
+                    images: [],
+                    price: 1234,
+                    discount: 0,
+                },
+            ],
+        };
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={ProductPage}
+                cart={{ id: 1, products: {} }}
+            />,
+        );
 
-    expect(screen.getByText('Cart (0)')).toBeInTheDocument();
-    fireEvent.click(screen.getByText('Add to bag'));
-    await waitFor(() => {
+        expect(screen.getByText('Cart (0)')).toBeInTheDocument();
+        fireEvent.click(screen.getByText('Add to bag'));
+        await waitFor(() => {
+            expect(screen.getByText('Cart (1)')).toBeInTheDocument();
+        });
+    });
+
+    test('Should display cart if cart modal is clicked', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+            }),
+        );
+
+        const pageProps = {
+            products: [
+                {
+                    id: 1,
+                    name: 'Product',
+                    description: 'Description',
+                    defaultImage: '',
+                    images: [],
+                    price: 1234,
+                    discount: 0,
+                },
+            ],
+        };
+
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={ProductPage}
+                cart={{
+                    id: 1,
+                    products: {
+                        1: {
+                            id: 1,
+                            name: 'Product',
+                            description: 'Description',
+                            price: 1234,
+                            quantity: 1,
+                        },
+                    },
+                }}
+            />,
+        );
+
         expect(screen.getByText('Cart (1)')).toBeInTheDocument();
+        fireEvent.click(screen.getByText('Cart (1)'));
+
+        await waitFor(() => {
+            expect(screen.getByText('Items in cart')).toBeInTheDocument();
+        });
     });
 });
 
-test('Should display cart if cart modal is clicked', async () => {
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-            ok: true,
-        }),
-    );
+describe('Search page page', () => {
+    test('Should display a button to open cart', () => {
+        const pageProps = { products: [] };
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={SearchPage}
+                cart={{ id: 1, products: {} }}
+            />,
+        );
 
-    const pageProps = {
-        products: [
-            {
-                id: 1,
-                name: 'Product',
-                description: 'Description',
-                defaultImage: '',
-                images: [],
-                price: 1234,
-                discount: 0,
-            },
-        ],
-    };
+        expect(screen.getByText('Cart (0)')).toBeInTheDocument();
+    });
 
-    render(
-        <MyApp
-            pageProps={pageProps}
-            Component={HomePage}
-            cart={{
-                id: 1,
-                products: {
-                    1: {
-                        id: 1,
-                        name: 'Product',
-                        description: 'Description',
-                        price: 1234,
-                        quantity: 1,
-                    },
+    test('Should add item to cart if "add to bag" is clicked', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+            }),
+        );
+
+        const pageProps = {
+            products: [
+                {
+                    id: 1,
+                    name: 'Product',
+                    description: 'Description',
+                    defaultImage: '',
+                    images: [],
+                    price: 1234,
+                    discount: 0,
                 },
-            }}
-        />,
-    );
+            ],
+        };
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={SearchPage}
+                cart={{ id: 1, products: {} }}
+            />,
+        );
 
-    expect(screen.getByText('Cart (1)')).toBeInTheDocument();
-    fireEvent.click(screen.getByText('Cart (1)'));
+        expect(screen.getByText('Cart (0)')).toBeInTheDocument();
+        fireEvent.click(screen.getByText('Add to bag'));
+        await waitFor(() => {
+            expect(screen.getByText('Cart (1)')).toBeInTheDocument();
+        });
+    });
 
-    await waitFor(() => {
-        expect(screen.getByText('Items in cart')).toBeInTheDocument();
+    test('Should display cart if cart modal is clicked', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+            }),
+        );
+
+        const pageProps = {
+            products: [
+                {
+                    id: 1,
+                    name: 'Product',
+                    description: 'Description',
+                    defaultImage: '',
+                    images: [],
+                    price: 1234,
+                    discount: 0,
+                },
+            ],
+        };
+
+        render(
+            <MyApp
+                pageProps={pageProps}
+                Component={SearchPage}
+                cart={{
+                    id: 1,
+                    products: {
+                        1: {
+                            id: 1,
+                            name: 'Product',
+                            description: 'Description',
+                            price: 1234,
+                            quantity: 1,
+                        },
+                    },
+                }}
+            />,
+        );
+
+        expect(screen.getByText('Cart (1)')).toBeInTheDocument();
+        fireEvent.click(screen.getByText('Cart (1)'));
+
+        await waitFor(() => {
+            expect(screen.getByText('Items in cart')).toBeInTheDocument();
+        });
     });
 });
